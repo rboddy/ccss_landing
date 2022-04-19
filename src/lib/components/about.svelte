f6152b5a169d65e19dfc1819890e6e41b16e0ff1
rboddy <avidryanb@gmail.com>
2022-04-18 23:37:48 -0400

    Adding info in about

--- src/lib/components/about.svelte
+++ src/lib/components/about.svelte
@@ -1,10 +1,32 @@
 <section>
   <div class="aboutContent">
     <h2>What's in the Book?</h2>
+    <ul>
+      <li>A brief history of CSS</li>
+      <li>The syntax of CSS</li>
+      <li>How to organize your CSS to maxmize efficiency</li>
+      <li>Responsive CSS</li>
+      <li>The Box Model</li>
+      <li>Pre and Post Processors</li>
+      <li>Industry Standard Frameworks</li>
+      <li>And much more</li>
+    </ul>
   </div>
 </section>
 
 <style>
+  ul {
+    list-style: url("images/bullet.png");
+    columns: 2;
+    -webkit-columns: 2;
+    -moz-columns: 2;
+    width: 80%;
+    margin: 0 auto;
+  }
+  li {
+    font-size: 2.5em;
+    font-family: "Caveat", cursive;
+  }
   .aboutContent {
     width: 80%;
     margin: 0 auto;
