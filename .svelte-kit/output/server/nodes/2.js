19b35940c75c722280bac18fa21ba98a8cfc9c00
rboddy <avidryanb@gmail.com>
2022-04-27 11:15:26 -0400

    More cleanup

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/index.svelte.js';
-
-export { module };
-export const entry = 'pages/index.svelte-aaef3bac.js';
-export const js = ["pages/index.svelte-aaef3bac.js","chunks/index-996e7094.js"];
-export const css = ["assets/pages/index.svelte-d4980e0f.css"];
