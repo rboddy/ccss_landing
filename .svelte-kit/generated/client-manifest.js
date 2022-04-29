86ae813e5b28db2ec0644d1d3e10f4add5973623
rboddy <avidryanb@gmail.com>
2022-04-28 21:50:02 -0400

    Adding the launch team sign up page.

--- .svelte-kit/generated/client-manifest.js
+++ .svelte-kit/generated/client-manifest.js
@@ -4,10 +4,12 @@ export const components = [
 	() => import("../runtime/components/layout.svelte"),
 	() => import("../runtime/components/error.svelte"),
 	() => import("../../src/routes/free.svelte"),
-	() => import("../../src/routes/index.svelte")
+	() => import("../../src/routes/index.svelte"),
+	() => import("../../src/routes/launch.svelte")
 ];
 
 export const dictionary = {
 	"": [[0, 3], [1]],
-	"free": [[0, 2], [1]]
+	"free": [[0, 2], [1]],
+	"launch": [[0, 4], [1]]
 };
